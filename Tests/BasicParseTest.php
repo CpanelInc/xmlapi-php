2ba0a4594523d29fb8e027cdb74c8f1ba91182db
David Neimeyer <david.neimeyer@cpanel.net>
2012-11-19 16:59:00 -0600

    Tidy Tests/* with php-cs-fixer

--- Tests/BasicParseTest.php
+++ Tests/BasicParseTest.php
@@ -4,19 +4,18 @@ require_once 'PHPUnit/Framework.php';
 error_reporting(E_WARNING);
 class BasicParseTest extends PHPUnit_Framework_TestCase
 {
-	/**
-	 * Silly test to simply confirm that it doesn't have parsing errors
-	 */
+    /**
+     * Silly test to simply confirm that it doesn't have parsing errors
+     */
     public function testXmlapiClassCanParse()
     {
         $classfile = '../xmlapi.php';
         $redirection = ' 2>/dev/null';
         $cmd = 'php -l '.$classfile. $redirection;
-        
+
         $last_line = exec($cmd, $lines);
         $clean = 'No syntax errors detected in '. $classfile;
-        
+
         $this->assertEquals($clean, $lines[0],'Syntax check found errors');
     }
 }
-?>
