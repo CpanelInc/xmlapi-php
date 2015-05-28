d4dd77e5fb1275fb1332b583477c8744ed30ca81
Julio Vinachi <julio899@gmail.com>
2015-05-28 11:23:01 -0430

    add config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+	/**************************
+	* General settings Access *
+	***************************/
+
+	define("ROOT_USER", "user_WHM");// user WHM
+	define("ROOT_PASSWORD", "PasswordXXX*");// password user WHM
+	define("SERVER_NAME", $_SERVER['SERVER_NAME']);// name the domain the server WHM, example: domain.com
+	define("SERVER_IP", $_SERVER['SERVER_ADDR']);// if the script is hosted on the same server whm. (No change) / ip xxx.xxx.xxx.xxx otherwise the WHM
+?>
